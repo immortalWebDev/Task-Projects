ec668649f2d1fd5a647cc1d9828dffa413a7d24d
immortalWebDev <pgbadgujar007@gmail.com>
2024-06-14 15:03:36 +0530

    Updated/added for cart add/remove

--- vite-redux-advanced/src/components/Shop/Products.jsx
+++ vite-redux-advanced/src/components/Shop/Products.jsx
@@ -1,16 +1,48 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 5,
+    title: 'My 1st book',
+    description: 'The first book I ever wrote'
+  },
+  {
+    id: 'p2',
+    price: 10,
+    title: 'My 2nd book',
+    description: 'The second book I ever wrote'
+  },
+  {
+    id: 'p3',
+    price: 15,
+    title: 'My 3rd book',
+    description: 'The third book I ever wrote'
+  },
+  {
+    id: 'p4',
+    price: 20,
+    title: 'My 4th book',
+    description: 'The fourth book I ever wrote'
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
         />
+        ))}
+        
       </ul>
     </section>
   );
