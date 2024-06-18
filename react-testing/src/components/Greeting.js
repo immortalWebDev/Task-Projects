dea1fb98f7c68f874e89f5bf2fbcb6315fe31916
immortalWebDev <pgbadgujar007@gmail.com>
2024-06-18 15:43:09 +0530

    Updated and added test cases for state changes

--- react-testing/src/components/Greeting.js
+++ react-testing/src/components/Greeting.js
@@ -1,12 +1,21 @@
-const Greeting = () => {
-    return(
-        <div>
-            <h1>
-                Hello world!
-            </h1>
-        <p>This is react testing project!</p>
-        </div>
-    )
-}
+import { useState } from 'react';
 
-export default Greeting
+const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      <p>This is react app testing</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
+    </div>
+  );
+};
+
+export default Greeting;
